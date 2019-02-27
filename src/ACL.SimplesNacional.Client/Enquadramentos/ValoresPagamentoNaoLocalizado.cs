b0be3a0919ccab62013b04b8f00ac603ba13ddd5
Edson Dewes <edson_dewes@hotmail.com>
2019-02-27 11:49:19 -0300

    Add "NumeroDasPag" e "NumeroParcelamento" aos valores de pag. não localizado

--- src/ACL.SimplesNacional.Client/Enquadramentos/ValoresPagamentoNaoLocalizado.cs
+++ src/ACL.SimplesNacional.Client/Enquadramentos/ValoresPagamentoNaoLocalizado.cs
@@ -8,9 +8,19 @@
         public string Dasd { get; set; }
 
         /// <summary>
+        /// Número do PAG utilizado para resolver o enquadramento
+        /// </summary>
+        public string NumeroDasPag { get; set; }
+
+        /// <summary>
         /// Número da guia devida
         /// </summary>
         public string NumeroGuia { get; set; }
+
+        /// <summary>
+        /// Número do parcelamento utilizado para resolver o enquadramento
+        /// </summary>
+        public int? NumeroParcelamento { get; set; }
 
         /// <summary>
         /// Valor da guia devida

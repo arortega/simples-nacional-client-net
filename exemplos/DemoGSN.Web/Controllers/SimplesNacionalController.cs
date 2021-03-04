166baf8a513e57f8ace0ae609658d4e34ad68373
Cassiano Kuss <cassianokuss@gmail.com>
2021-03-04 16:19:41 -0300

    feat: Criado exemplo em ASP.Net MVC

---
+++ exemplos/DemoGSN.Web/Controllers/SimplesNacionalController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ACL.SimplesNacional.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoGSN.Web.Controllers
+{
+    public class SimplesNacionalController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var claims = new Dictionary<string, string>
+            {
+                { "role" , "sn-contrib" },
+                { "name" , "Usuário Demonstração" },
+                { "matricula" , "123456"},
+            };
+
+            var token = await new SimplesNacionalClient("https://auth.aclti.com.br/", "webiss5_5915", "KacU5707*k").ObterUserToken("11111111111", "senha", claims);
+
+            ViewData.Add("accessToken", token.Access);
+            ViewData.Add("claims", token.Claims);
+            ViewData.Add("Versao", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+            ViewData.Add("ApiAnalise", "https://simplesnacional-treinamento.aclti.com.br/api/fiscalizacao");
+            ViewData.Add("ApiFiscalizacao", "https://simplesnacional-treinamento.aclti.com.br/api/fiscalizacao");
+            ViewData.Add("ApiDTE", "https://simplesnacional-treinamento.aclti.com.br/api/dte");
+            ViewData.Add("ApiImportacaoGerencial", "https://simplesnacional-treinamento.aclti.com.br/api/importacaoGerencial");
+
+            return View();
+        }
+    }
+}
